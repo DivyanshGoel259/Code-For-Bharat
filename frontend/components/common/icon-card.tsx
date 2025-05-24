b3d2bdb9f067d34a86a5c05be4bad0cbf72f4571
Divyansh Goel <divyanshgoel8860@gmail.com>
2025-05-24 12:19:19 +0530

    First Commit

---
+++ frontend/components/common/icon-card.tsx
@@ -0,0 +1,28 @@
+import { IconArgs } from "@/types";
+
+export const IconCard = ({
+  icon: Icon,
+  iconText,
+  title,
+  titleSize,
+  iconTextSize,
+  iconColor,
+  titleColor,
+}: IconArgs) => {
+  return (
+    <div>
+      <div>
+        <div className="p-2 flex flex-col justify-center items-center ">
+          <div className="h-full w-full">
+          <Icon ></Icon>
+          </div>
+          
+        </div>
+        <div className={` text-center ${iconTextSize} ${iconColor}`}>
+          {iconText}
+        </div>
+        <div className={`mt-2 text-center ${titleSize} ${titleColor}`}>{title}</div>
+      </div>
+    </div>
+  );
+};
---
+++ frontend/types.ts
@@ -0,0 +1,11 @@
+import { ElementType } from "react"
+
+export interface IconArgs {
+    icon:ElementType
+    iconText:string,
+    title:string,
+    titleSize:string,
+    iconTextSize:string,
+    iconColor:string,
+    titleColor:string
+}
