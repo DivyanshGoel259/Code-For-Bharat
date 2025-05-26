2df669e7a6f3db0a0b68a46739a9a2148c4b3e78
Poorva Gupta <136830386+poorvagupta2003@users.noreply.github.com>
2025-05-27 01:34:37 +0530

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,18 @@
 import { AboutSection } from "@/components/Home/about-section";
 import Image from "next/image";
+import DigitalSwag from "@/components/Home/digitalswag";
+import PrizePool from "@/components/Home/prizepool";
+import Sponsors from "@/components/Home/sponsors";
+import FAQ from "@/components/Home/faqs";
 
 export default function Home() {
   return (
     <div>
       <AboutSection></AboutSection>
+      <DigitalSwag/>
+      <PrizePool/>
+      <Sponsors/>
+      <FAQ/>
     </div>
   );
 }
