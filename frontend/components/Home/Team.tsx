3b6b45a29f5c4827d4532f6d23975eb1416d2982
Divyansh Goel <divyanshgoel8860@gmail.com>
2025-05-27 12:21:11 +0530

    Add Team section and integrate Framer Motion

--- frontend/components/Home/Team.tsx
+++ frontend/components/Home/Team.tsx
@@ -1,12 +1,59 @@
-export const Team = ()=>{
-    return <div className="h-screen flex flex-col justify-center items-center bg-slate-800">
-        <div>
-            <div className="text-5xl font-bold text-white ">
-                Judges & Mentors
-            </div>
-            <div>
-                
-            </div>
+"use client"
+
+import { teamCards } from "@/constants";
+import { TeamCard } from "../common/team-card";
+import { motion } from "framer-motion";
+
+export const Team = () => {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-slate-800">
+      <div>
+        {/* Judges & Mentors Heading */}
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+  whileInView={{ opacity: 1, y: 0 }}
+  viewport={{ once: false, amount: 0.5 }}
+  transition={{ duration: 0.6, ease: "easeOut" }}
+  className="font-playfair text-center text-4xl font-bold text-white"
+>
+          Judges & Mentors
+        </motion.div>
+
+        <div className="p-2 mt-6 flex gap-5">
+          {teamCards.map((payload) => (
+            <TeamCard
+              key={payload.id}
+              imageUrl={payload.imageUrl}
+              information={payload.information}
+              name={payload.name}
+            />
+          ))}
         </div>
+      </div>
+
+      <div>
+        {/* Lead Organizers Heading */}
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+  whileInView={{ opacity: 1, y: 0 }}
+  viewport={{ once: false, amount: 0.5 }}
+  transition={{ duration: 0.6, ease: "easeOut" }}
+  className="mt-6 font-playfair text-center text-4xl font-bold text-white"
+>
+          Lead Organizers
+        </motion.div>
+
+        <div className="p-2 mt-6 flex gap-5">
+          {teamCards.map((payload) => (
+            <TeamCard
+              key={payload.id}
+              imageUrl={payload.imageUrl}
+              information={payload.information}
+              name={payload.name}
+            />
+          ))}
+        </div>
+      </div>
     </div>
-}
+  );
+};
