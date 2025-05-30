85c313275adfc991ad9662faac747b6afca4d548
Poorva Gupta <136830386+poorvagupta2003@users.noreply.github.com>
2025-05-30 17:35:34 +0530

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,5 @@
+import LoadingScreen from './components/LoadingScreen';
+import SideNavigation from './components/SideNavigation';
 import { AboutSection } from "@/components/Home/about-section";
 import { Team } from "@/components/Home/Team";
 import { Tracks } from "@/components/Home/tracks";
@@ -6,6 +8,7 @@ import DigitalSwag from "@/components/Home/digitalswag";
 import PrizePool from "@/components/Home/prizepool";
 import Sponsors from "@/components/Home/sponsors";
 import FAQ from "@/components/Home/faqs";
+
 
 export default function Home() {
   return (
